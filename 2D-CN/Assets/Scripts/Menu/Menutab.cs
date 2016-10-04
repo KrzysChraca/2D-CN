65217fc1c44f47f38f3eabc6a65b6e44c349535a
KrzysChraca <krzyschraca@gmail.com>
2016-10-03 22:11:12 -0500

    Menu Stuff

---
+++ 2D-CN/Assets/Scripts/Menu/Menutab.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuTab : MonoBehaviour {
+
+    public Transform MenuTabSelectable;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+   
+
+    public void ToggleMenuTab()
+    {
+        if (MenuTabSelectable.gameObject.activeInHierarchy == false)
+        {
+            MenuTabSelectable.gameObject.SetActive(true);
+        }
+        else
+        {
+            MenuTabSelectable.gameObject.SetActive(false);
+        }
+    }
+    
+}
---
+++ 2D-CN/Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Transform canvas;
+	
+	// Update is called once per frame
+	void Update () {
+        ScanForKeyStroke();
+    }
+
+    void ScanForKeyStroke()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            TogglePauseMenu();
+            
+    }
+
+    public void TogglePauseMenu()
+    {
+        if(canvas.gameObject.activeInHierarchy == false)
+        {
+            canvas.gameObject.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            canvas.gameObject.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
