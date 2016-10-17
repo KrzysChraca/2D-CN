f80991b4ea14245c1b9cde30abaf8300fc830b78
KrzysChraca <krzyschraca@gmail.com>
2016-10-16 22:02:59 -0500

    update

---
+++ 2D-CN/Assets/Scripts/Projectiles/Bullet2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet2 : Projectile
+{
+
+    void Awake()
+    {
+        damage = 20;
+        deathTimer = 1;
+        speed = 55;
+        fireRate = 0.1f;
+        StartCoroutine(DeathTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += (Time.deltaTime * speed) * moveDir;
+    }
+}
